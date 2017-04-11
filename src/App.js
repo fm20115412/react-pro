711bea9e3eb6402849b1e97303d2dba1d433e3fa
fm20115412 <641290900@qq.com>
2017-04-11 20:59:31 +0800

    子元素调用父元素的方法

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import "normalize.css"
+import "./reset.css"
 import TodoInput from "./TodoInput";
 import TodoItem from "./TodoItem";
 
@@ -7,27 +9,27 @@ class App extends Component {
   constructor(props){
       super(props)
       this.state={
-          newTodo:"test",
+          newTodo:"",
           todoList:[
-              { id:1, title:"第一个待办"},
-              { id:2, title:"第二个待办"},
-              { id:3, title:"第三个待办"}
           ]
       }
   }
   render(){
       let todos=this.state.todoList.map((item,index)=>{
           return(
-              <li>
+              <li key={index}>
                   <TodoItem todo={item}/>
               </li>
           )
       })
+      console.log(todos)
       return (
           <div className="App">
              <h1>我的待办</h1>
               <div className="inputWrapper">
-                  <TodoInput content={this.state.newTodo}/>
+                  <TodoInput content={this.state.newTodo}
+                             onChage={this.changeTitle.bind(this)}
+                             onSubmit={this.addTodo.bind(this)}/>
               </div>
                 <ol>
                     {todos}
@@ -35,5 +37,31 @@ class App extends Component {
           </div>
       )
   }
+  addTodo(){
+      this.state.todoList.push({
+          id:idMaker(),
+          title:event.target.value,
+          status:null,
+          deleted:false
+      })
+      this.setState({
+            newTodo:"",
+            toloList:this.state.todoList
+      })
+  }
+  changeTitle(){
+      this.setState({
+          newTodo:event.target.value,
+          todoList:this.state.todoList
+
+      })
+  }
+
+
 }
 export default App;
+let id=0;
+function idMaker(){
+    id+=1;
+    return id;
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
+import App from './App';
 import './index.css';
 import Welcome from "./Welcome.js"
 ReactDOM.render(
-    <Welcome name="babybear"/>,
+    <App/>,
     document.getElementById('root')
 );
 

c44277abb93f58ff0f3cc0c58e59ebd35de761e4
fm20115412 <641290900@qq.com>
2017-04-11 15:13:32 +0800

    基本骨架完成

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import './index.css';
-
+import Welcome from "./Welcome.js"
 ReactDOM.render(
-  <h1>hello world</h1>,
-  document.getElementById('root')
+    <Welcome name="babybear"/>,
+    document.getElementById('root')
 );
+
+
+
